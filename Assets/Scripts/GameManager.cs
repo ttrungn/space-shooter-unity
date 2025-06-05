3312433d79c5e8bf661177cdeb9629a0b2936602
ttrungn <trungnguyen0803forwork@gmail.com>
2025-06-05 16:58:38 +0700

    finish basic function for 2d space game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+using UnityEditor;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject enemyPrefaf;
+    public float minInitiateValue;
+    public float maxInitiateValue;
+    public float enemyDestroyTime = 10f;
+
+    [Header("Particle Effects")]
+    public GameObject explosion;
+    public GameObject muzzleFlash;
+
+    [Header("Panels")]
+    public GameObject startMenu;
+    public GameObject pauseMenu;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        startMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 0f;
+        InvokeRepeating("InstantiateEnemy", 1f, 2f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    void InstantiateEnemy()
+    {
+        var enemyPos = new Vector3(Random.Range(minInitiateValue, maxInitiateValue), 6f);
+        var enemy = Instantiate(enemyPrefaf, enemyPos, Quaternion.Euler(0f, 0f, 180f));
+        Destroy(enemy, enemyDestroyTime);
+    }
+
+    public void InstantiateParticles(GameObject gameObject, Vector3 position, Quaternion rotation)
+    {
+        var gm = Instantiate(gameObject, position, rotation);
+        Destroy(gm, 2f);
+    }
+
+    public void StartGameButton()
+    {
+        startMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    [Header("Missile")]
+    public GameObject missile;
+    public Transform missileSpawnPosition;
+    public Transform muzzleSpawnPosition;
+    public float destroyTime = 1f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        PlayerMove();
+        PlayerShoot();
+    }
+
+    void PlayerMove()
+    {
+        var xPos = Input.GetAxis("Horizontal");
+        var yPos = Input.GetAxis("Vertical");
+        var movement = new Vector3(xPos, yPos, 0) * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+    void PlayerShoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnMuzzleFlash();
+            SpawnMissile();
+        }
+    }
+
+    void SpawnMuzzleFlash()
+    {
+        var muzzleObject = Instantiate(GameManager.instance.muzzleFlash, muzzleSpawnPosition);
+        muzzleObject.transform.SetParent(null);
+        Destroy(muzzleObject, destroyTime);
+    }
+
+    void SpawnMissile()
+    {
+        var missileObject = Instantiate(missile, missileSpawnPosition);
+        missileObject.transform.SetParent(null);
+        Destroy(missileObject, destroyTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
