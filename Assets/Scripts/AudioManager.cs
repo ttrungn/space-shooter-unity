fec73f95f8744c09e4ea3e4d11d955737f7828a3
ttrungn <trungnguyen0803forwork@gmail.com>
2025-06-20 11:24:35 +0700

    add sounds and controller menu background

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [Header("Audio Clip")]
+    public AudioClip background;
+    public AudioClip shoot;
+
+    void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
